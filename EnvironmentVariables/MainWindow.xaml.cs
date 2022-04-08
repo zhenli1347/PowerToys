1694a77f95ffecace346ca4ded29c542dad54345
Niels Laute <niels.laute@live.nl>
2022-04-08 15:05:56 +0200

    UI tweaks

--- EnvironmentVariables/MainWindow.xaml.cs
+++ EnvironmentVariables/MainWindow.xaml.cs
@@ -12,20 +12,43 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI;           // Needed for WindowId
+using Microsoft.UI.Windowing; // Needed for AppWindow
+using WinRT.Interop;          // Needed for XAML/HWND interop.
 
 namespace EnvironmentVariables
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+
+        private AppWindow m_AppWindow;
+
         public MainWindow()
         {
             this.InitializeComponent();
+            m_AppWindow = GetAppWindowForCurrentWindow();
+            // Check to see if customization is supported.
+            // Currently only supported on Windows 11.
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = m_AppWindow.TitleBar;
+                // Hide default title bar.
+                titleBar.ExtendsContentIntoTitleBar = true;
+            }
+            else
+            {
+                // Title bar customization using these APIs is currently
+                // supported only on Windows 11. In other cases, hide
+                // the custom title bar element.
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
         }
     }
 }
