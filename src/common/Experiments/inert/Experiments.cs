ddf44852421c7459b1e50159b81b879d7c511208
Sophia Chen <sophia.six.chen@gmail.com>
2022-11-28 09:31:30 -0800

    inital commit

---
+++ src/common/Experiments/inert/Experiments.cs
@@ -0,0 +1,16 @@
+﻿namespace inert
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Experiments
+    {
+        public static bool EnableLandingPageExperiment()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/common/Experiments/live/Experiments.cs
@@ -0,0 +1,18 @@
+﻿namespace live
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Experiments
+    {
+        public static bool EnableLandingPageExperiment()
+        {
+            // Do experiment stuff
+
+            return true;
+        }
+    }
+}
